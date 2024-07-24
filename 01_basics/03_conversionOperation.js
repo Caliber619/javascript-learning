217c04b14ca3e8b6d7f56a38b2e88a7820427433
Kshitij Dev <101935676+Caliber619@users.noreply.github.com>
2024-07-24 19:53:09 +0000

    conversion of data types

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,29 @@
+let score = 33
+let score2 = true
+
+console.log(typeof score);
+console.log(typeof (score2));
+
+let valueInNumber = Number(score2);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+//"33" => 33
+// "33abc" => NaN
+// true =>1; false =>0
+
+let isLoggedIn = 0;
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn)
+
+// 1 => true; 0=> false
+// "" => false
+// "Kshitij" => true
+
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber)
+console.log(stringNumber)
