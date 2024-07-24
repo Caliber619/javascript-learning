594196a00517c29eacb7bc810eab201ba1f26899
Kshitij Dev <101935676+Caliber619@users.noreply.github.com>
2024-07-24 19:02:54 +0000

    new code

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 144553
+let accountEmail = "kshitij@gmail.com"
+var accountPassword = "12345"
+accountCity = "jaipur"
+
+// accountId = 2 //not allowed
+accountEmail = "kd@gmail.com"
+accountPassword = "1123"
+accountCity = "jhansi"
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword, accountCity]);
