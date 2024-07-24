98b0f4314a8451bdd4ed140b38dbfb91c86d3193
Kshitij Dev <101935676+Caliber619@users.noreply.github.com>
2024-07-24 19:31:14 +0000

    datatypes understood

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,30 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3+3)  // we are using nodejs, not browser
+
+console.log(3+3); //code readability should be high
+console.log("Kshitij")
+
+
+let name = "Kshitij"
+let age = 21
+let isLoggedIn = false
+
+
+//number => 2 to power 53
+//bigint
+//string => " "
+//boolean => true/false
+//null => standalone value (not undefined)
+//undefined => not defined
+//symbol => unique
+
+
+//object
+
+
+
+console.log(typeof name)
+console.log(typeof age)
+console.log(typeof null)
+console.log(typeof undefined)
