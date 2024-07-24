a0dd7364e0121a08c5947f0c5069726662353e21
Kshitij Dev <101935676+Caliber619@users.noreply.github.com>
2024-07-24 20:13:33 +0000

    conversion operation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -27,3 +27,33 @@ let someNumber = 33
 let stringNumber = String(someNumber)
 console.log(typeof stringNumber)
 console.log(stringNumber)
+
+
+
+// *************************** Operations ****************************
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+let str1 = "hello"
+let str2 = "Kshitij"
+
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1"+2);
+console.log(1+ "2");
+console.log("1"+"2");
+console.log("1"+2+2);
+console.log(1+2+"2")
+
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2+2
+
+let gameCounter = 100;
+++gameCounter;
+
+console.log(gameCounter);
