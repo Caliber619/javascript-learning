36a1cb80a096cd1d31af31f6fcacfee102d8160f
Kshitij Dev <101935676+Caliber619@users.noreply.github.com>
2024-07-24 19:11:46 +0000

    variables and constants understood

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -2,6 +2,7 @@ const accountId = 144553
 let accountEmail = "kshitij@gmail.com"
 var accountPassword = "12345"
 accountCity = "jaipur"
+let accountState;
 
 // accountId = 2 //not allowed
 accountEmail = "kd@gmail.com"
@@ -10,4 +11,8 @@ accountCity = "jhansi"
 
 console.log(accountId);
 
-console.table([accountId,accountEmail,accountPassword, accountCity]);
+/*
+prefer not to use "var" because of issue in block and functional scope
+*/
+
+console.table([accountId,accountEmail,accountPassword, accountCity, accountState]);
