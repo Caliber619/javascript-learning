947422f971c1da69bbc562ee2621d2271d46e55e
Kshitij Dev <101935676+Caliber619@users.noreply.github.com>
2024-07-28 19:30:59 +0000

    date and time

---
+++ 01_basics/07_dateAndTime.js
@@ -0,0 +1,12 @@
+//Dates
+
+let myDate = new Date()
+console.log(myDate);     //not readable
+
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toISOString());
+console.log(myDate.toJSON());
+console.log(myDate.toLocaleDateString());
+console.log(myDate.toLocaleString());
+
