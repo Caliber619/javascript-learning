eb4e9de14f580aa401f225346fbe78d704d1d5ed
Kshitij Dev <101935676+Caliber619@users.noreply.github.com>
2024-07-28 20:20:14 +0000

    date and time

--- 01_basics/07_dateAndTime.js
+++ 01_basics/07_dateAndTime.js
@@ -10,3 +10,31 @@ console.log(myDate.toJSON());
 console.log(myDate.toLocaleDateString());
 console.log(myDate.toLocaleString());
 
+let myCreatedDate = new Date(2023, 0,23);
+console.log(myCreatedDate.toDateString());
+
+let myDate2 = new Date("2023-01-14")
+console.log(myDate2.toLocaleString());
+
+//timestamps below
+let myTimeStamp = Date.now()
+console.log(myTimeStamp)
+console.log(myCreatedDate.getTime());
+console.log(Math.floor(Date.now()/1000));
+
+
+let newDate = new Date()
+console.log(newDate);
+
+console.log(newDate.getDate())
+console.log(newDate.getDay())
+
+
+console.log(`${newDate.getDay()} and the time`)//string Interpolation
+
+
+newDate.toLocaleString('default',{
+    weekday: "long",
+
+})
+console.log(newDate)
