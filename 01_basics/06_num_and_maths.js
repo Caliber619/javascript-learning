a7a22776b8d537d5d7a2321efc68408e6a4b0be6
Kshitij Dev <101935676+Caliber619@users.noreply.github.com>
2024-07-28 19:08:06 +0000

    nums and math

--- 01_basics/06_num_and_maths.js
+++ 01_basics/06_num_and_maths.js
@@ -10,3 +10,24 @@ console.log(balance.toFixed(1))
 const otherNumber = 123.8966
 
 console.log(otherNumber.toPrecision(3));
+
+const hundreds = 100000
+console.log(hundreds.toLocaleString('en-IN'));
+
+
+//**********************MATHS************************* */
+
+console.log(Math);   //shows it's an object
+console.log(Math.abs(-4));
+console.log(Math.round(12.2));
+console.log(Math.ceil(12.2));
+console.log(Math.floor(12.2));
+
+console.log(Math.random()); //humesha value 0 and 1 
+console.log(Math.random()*10);
+console.log(Math.floor(Math.random()*10) + 1); // zero aane se rokne ke liye 1 add kia
+
+const min = 10
+const max = 20//indono ke beech ka chahiye
+
+console.log(Math.floor(Math.random() * (max-min +1)) + min)//max and min ke beech me aaye but min se zada to aae hi aae
