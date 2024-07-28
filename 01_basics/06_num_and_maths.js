0c96af3665499376283a30c6cb824792f5447556
Kshitij Dev <101935676+Caliber619@users.noreply.github.com>
2024-07-28 18:05:46 +0000

    nums and maths

---
+++ 01_basics/06_num_and_maths.js
@@ -0,0 +1,12 @@
+const score = 400
+console.log(score);
+
+const balance = new Number(100)
+console.log(balance)
+
+console.log(balance.toString().length)
+console.log(balance.toFixed(1))
+
+const otherNumber = 23.8966
+
+console.log(otherNumber.toPrecision(3));
