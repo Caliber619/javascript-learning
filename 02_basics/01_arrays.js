b4b52a6753878bc5e2dd20afd88d1c758109e312
Kshitij Dev <101935676+Caliber619@users.noreply.github.com>
2024-07-29 19:31:10 +0000

    arrays1

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -1,29 +1,42 @@
 // arrays
 
 const myArr = [0,1,2,3,4,5]
-console.log(myArr[0])
+// console.log(myArr[0])
 
-const myHeros = ["shaktiman", "naagraj"]
+// const myHeros = ["shaktiman", "naagraj"]
 
-const myArr2 = new Array(1,2,3,4)
-console.log(myArr2[2])
+// const myArr2 = new Array(1,2,3,4)
+// console.log(myArr2[2])
 
-// array methods
-myArr.push(6)
-console.log(myArr)
-myArr.pop()
+// // array methods
+// myArr.push(6)
+// console.log(myArr)
+// myArr.pop()
 
-console.log(myArr)
+// console.log(myArr)
 
-myArr.unshift(9)
-console.log(myArr);
-myArr.shift()
-console.log(myArr)
+// myArr.unshift(9)
+// console.log(myArr);
+// myArr.shift()
+// console.log(myArr)
 
 
-console.log(myArr.includes(9));
-console.log(myArr.indexOf(3))
+// console.log(myArr.includes(9));
+// console.log(myArr.indexOf(3))
 
 
 const newArr = myArr.join()
-console.log(myArr); 
+
+console.log(myArr);
+console.log(typeof newArr);
+
+// slice, splice
+console.log("A", myArr)
+
+const myn1 = myArr.slice(1,3);
+console.log(myn1);
+console.log(myArr)
+console.log("B",myArr)
+const myn2 = myArr.splice(1,3)
+console.log("B",myArr)
+console.log(myn2)
