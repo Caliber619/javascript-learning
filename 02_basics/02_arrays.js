47667267a0deed288d1077953a31e06ff208c89f
Kshitij Dev <101935676+Caliber619@users.noreply.github.com>
2024-08-01 19:42:20 +0000

    arrays

--- 02_basics/02_arrays.js
+++ 02_basics/02_arrays.js
@@ -12,3 +12,20 @@ console.log("hello")
 
 const all_new_heros = [...marvel_heros, ...dc_heros]
 console.log(all_new_heros)
+
+const another_array = [1,2,3,[4,5,6],7,[6,7,[4,5]]]
+
+const real_another_array = another_array.flat(Infinity)
+console.log(real_another_array)
+
+
+//to convert different formats into array
+console.log(Array.isArray("KSHITIJ"))
+console.log(Array.from("KSHITIJ"))
+console.log(Array.from({name:"KSHITIJ"})) //interesting
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+console.log(Array.of(score1,score2,score3))
