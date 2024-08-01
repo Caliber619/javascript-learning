b0209b29113dd56d24d8a98c52a8ad3cd169e017
Kshitij Dev <101935676+Caliber619@users.noreply.github.com>
2024-08-01 17:46:07 +0000

    arrays 2

--- 02_basics/02_arrays.js
+++ 02_basics/02_arrays.js
@@ -1,3 +1,11 @@
-const marvel_heros = ["thor","ironMan","spiderMan"]
-const dc_heros = ["sperman","badman","flashLight"]
+const marvel_heros = ["thor","ironMan","spiderMan"];
+const dc_heros = ["sperman","badman","flashLight"];
 
+marvel_heros.push(dc_heros)  //will push array2 into array1 as a data
+
+// console.log(marvel_heros)
+
+//instead this is how we should merge
+marvel_heros.concat(dc_heros);
+console.log(marvel_heros);
+console.log("hello")
