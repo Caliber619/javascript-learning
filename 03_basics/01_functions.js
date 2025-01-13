a1e03eda1a61272cbdf47a20d97deddd0e332bae
Kshitij Dev <101935676+Caliber619@users.noreply.github.com>
2025-01-13 12:28:05 +0000

    functions new topics

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -34,15 +34,15 @@ function addTwoNumbersNew(num1,num2){ //num1 and num2 are parameters
     console.log("Kshitij")        // won't print because result is already returned
 
 }
-console.log(addTwoNumbersNew(5,5))
-const ans = addTwoNumbersNew(5,4)
-console.log(ans)
+// console.log(addTwoNumbersNew(5,5))
+// const ans = addTwoNumbersNew(5,4)
+// console.log(ans)
 
 
 function loginUserMessage(username){
     return `${username} just logged in`
 }
-console.log(loginUserMessage("Kshitij"))
+// console.log(loginUserMessage("Kshitij"))
 
 
 function loginUserMessageNew(username){
@@ -52,3 +52,42 @@ function loginUserMessageNew(username){
     }
     return `${username} just logged in`
 }
+// console.log(loginUserMessageNew())     // will return undefined
+
+
+//shopping cart issue and function type 
+function calculateCartPrice(...num){
+    //can be written as (val1, val2, ...num)
+    return num
+}
+// console.log(calculateCartPrice(200,400,500))   //will put/retur a array   
+//on passing multivalues use rest operator(same as spread ...)
+
+
+const user = {
+    username: "caliber",
+    price: 199
+}
+//or
+handleObject(
+    {
+        username: "sam",
+        price: 399
+    }
+)
+
+function handleObject(anyObject){
+    console.log(`Username is ${anyObject.username} and price is ${anyObject.price}`);
+}
+// handleObject(user)
+
+
+
+//now let's see arrays
+const myNewArray = [200,300,100,599]
+function returnSecondValue(anArray){
+    return anArray[1]
+}
+console.log(returnSecondValue(myNewArray))
+//or
+console.log(returnSecondValue([200,300,100,599]))
