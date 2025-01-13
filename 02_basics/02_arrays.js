11d622bf5e3592a73d5918165b458521ab5d9f50
Kshitij Dev <101935676+Caliber619@users.noreply.github.com>
2025-01-13 07:20:45 +0000

    updated notes of arrays

--- 02_basics/02_arrays.js
+++ 02_basics/02_arrays.js
@@ -10,20 +10,26 @@ const newArr = marvel_heros.concat(dc_heros);
 console.log(newArr);
 console.log("hello")
 
+
+//spread operator ...(why? because concat has a limitation of only one argument)
 const all_new_heros = [...marvel_heros, ...dc_heros]
 console.log(all_new_heros)
 
+
+//flat function (give depth in brackets)
 const another_array = [1,2,3,[4,5,6],7,[6,7,[4,5]]]
 
 const real_another_array = another_array.flat(Infinity)
 console.log(real_another_array)
 
-
+//from function (Ex. ek string se array bana do Array.from("kshitij"))
 //to convert different formats into array
 console.log(Array.isArray("KSHITIJ"))
 console.log(Array.from("KSHITIJ"))
 console.log(Array.from({name:"KSHITIJ"})) //interesting
 
+
+// "of" function (Ex. kis kis ka array banana hai? Array.of(score1,score2))
 let score1 = 100
 let score2 = 200
 let score3 = 300
