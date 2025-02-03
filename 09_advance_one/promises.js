4db39cf5beda43a5253929dd0c6066f4d9d07134
Kshitij Dev <101935676+Caliber619@users.noreply.github.com>
2025-02-03 14:17:43 +0000

    promises completed

--- 09_advance_one/promises.js
+++ 09_advance_one/promises.js
@@ -37,48 +37,72 @@
 
 //----------------------------------------------------------
 
-const promiseFour = new Promise(function(resolve,reject){
-    setTimeout(function(){
-        let error = false
-        if(!error){
-            resolve({username: "kshitij", password: "123"})
-        }else{
-            reject('ERROR: something went wrong')
-        }
-    }, 1000)
-})
-promiseFour.then((user)=>{
-    console.log(user);
-    return user.username
-})
-.then((username) => {
-    console.log(username);
-})
-.catch(function(error){
-    console.log(error);
-})
-.finally(() => console.log("The promise is either resolved or rejected"))
+// const promiseFour = new Promise(function(resolve,reject){
+//     setTimeout(function(){
+//         let error = false
+//         if(!error){
+//             resolve({username: "kshitij", password: "123"})
+//         }else{
+//             reject('ERROR: something went wrong')
+//         }
+//     }, 1000)
+// })
+// promiseFour.then((user)=>{
+//     console.log(user);
+//     return user.username
+// })
+// .then((username) => {
+//     console.log(username);
+// })
+// .catch(function(error){
+//     console.log(error);
+// })
+// .finally(() => console.log("The promise is either resolved or rejected"))
 
 //----------------------------------------------------------
 
 
-const promiseFive = new Promise(function(resolve, reject){
-    setTimeout(function(){
-        let error = true
-        if (!error) {
-            resolve({username: "javascript", password: "123"})
-        } else {
-            reject('ERROR: JS went wrong')
-        }
-    }, 1000)
-});
-async function consumePromiseFive(){
-    try {
-        const response = await promiseFive
-        console.log(response);
-    } catch (error) {
-        console.log(error);
-    }
-}
+// const promiseFive = new Promise(function(resolve,reject){
+//     setTimeout(function(){
+//         let error = true;
+//         if(!error){
+//             resolve({username:"javascript", password: "123"})
+//         }else{
+//             reject('ERROR,Js not found')
+//         }
+//     }, 1000)
+// });
+// //---dusri tarah se handle krege promise   (but async await error handle nahi krte so wrap krna pdega try catch block me)
+// async function consumePromiseFive(){
+//     try {
+//         const response = await promiseFive
+//         console.log(response);
+//     } catch (error) {
+//         console.log(error)
+//     }
+// }
+// consumePromiseFive()
 
-consumePromiseFive()
+
+// ----------------------------------------------------------
+
+// async function getAllUsers(){
+//     try {
+//         const response = await fetch('https://api.github.com/users/hiteshchoudhary')
+//         const data = await response.json()
+//         console.log(data);
+//     } catch (error) {
+//         console.log("E: "+error);   
+//     }
+// }
+// getAllUsers()
+
+
+//-----------------------------------------------------
+
+fetch('https://api.github.com/users/hiteshchoudhary').then((response)=>{
+    return response.json()
+}).then((data)=>{console.log(data);
+}).catch((error)=>{
+    console.log("E: "+  error);
+})
