a06ee361689d212aa991733b04dc6f2c78f7487e
Kshitij Dev <101935676+Caliber619@users.noreply.github.com>
2025-02-04 20:11:54 +0000

    call and this in js

--- 10_classes_and_oop/call.js
+++ 10_classes_and_oop/call.js
@@ -1,4 +1,17 @@
 function setUserName(username){
     // complex db calls
     this.username = username
+    console.log("called");
 }
+
+function createUser(username,email,password){
+    // setUserName(username)  //method use nahi hora or call nahi hora hai
+    // setUserName.call(username) //isse call to hogya but reference hold nahi hua
+    setUserName.call(this, username)
+    this.email = email;
+    this.password = password;
+
+}
+
+const newUser = new createUser("caliber","cali@gmail.com","123")
+console.log(newUser);
